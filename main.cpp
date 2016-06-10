75880701fbf0bfc17b51d7ff3bb9f90b4667bfef
Grabarchuk Viktor <grabarchukviktor@gmail.com>
2016-06-11 01:00:26 +0300

    UTF-8 bug fixed

--- main.cpp
+++ main.cpp
@@ -6,26 +6,18 @@
 
 class Dictionary {
 private:
-    std::map<size_t, std::set<std::string>> wordsets;
+    std::map<size_t, std::set<std::wstring>> wordsets;
 public:
     Dictionary(const std::string &file_name)
     {
-        std::ifstream input(file_name);
+        std::wifstream input(file_name);
+        input.imbue(std::locale("en_US.UTF-8"));
         if(input.is_open())
         {
-            for (std::string word; std::getline(input, word);)
+            for (std::wstring word; std::getline(input, word);)
             {
                 std::transform(word.begin(), word.end(), word.begin(), ::tolower);
                 wordsets[word.length()].insert(word);
-            }
-
-            for(auto wordset : wordsets)
-            {
-                std::cout << wordset.first << std::endl;
-                for(auto word : wordset.second)
-                {
-                    std::cout << "\t" << word << std::endl;                    
-                }
             }
         }
     }
@@ -33,6 +25,7 @@ public:
 
 int main()
 {
+    std::setlocale(LC_ALL, "en_US.UTF-8");
     //std::string dictionary_file_name = "/usr/share/dict/words";
     std::string dictionary_file_name = "/home/dhmhd/Projects/elephant-fly/words";
     Dictionary dictionary(dictionary_file_name);
